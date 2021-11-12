e9fae64290ec1551ee5044e2d80c73aeaf4ec823
mahaamesha <mahaamesha@gmail.com>
2021-11-12 17:29:56 +0700

    fungsi ikutLine()

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,15 +1,49 @@
 #include "Robot.h"
 
+const byte lineL = 12;  //sensor garis kiri
+const byte lineR = 11;  //sensor garis kanan
+
 Robot bot(3,2,4,5,6,7);    // objek bot(ENA, IN1, IN2, ENB, IN3, IN4)
+void ikutLine();
 
 void setup() {
   Serial.begin(9600);
+  
+  pinMode(lineL, INPUT);
+  pinMode(lineR, INPUT);
+  
   bot.aturSpeed(60);  // speed X %
 }
 
 void loop() {
-  bot.maju(1000);
-  delay(1000);
-  bot.putarKiri(1000);
-  delay(1000);
+  ikutLine();
+}
+
+void ikutLine(){
+  byte valLineL = digitalRead(lineL); // 0 -> putih
+  byte valLineR = digitalRead(lineR); // 1 -> hitam
+
+  Serial.print("L: ");
+  Serial.println(valLineL);
+  Serial.print("R: ");
+  Serial.println(valLineR);
+  
+  if (valLineL + valLineR == 0){
+    Serial.println("MAJU");
+    bot.maju(2);
+  }
+  else if(valLineL + valLineR == 2){
+    Serial.println("STOP");
+    bot.stop(2);
+  }
+  else if(valLineL == 1 && valLineR == 0){
+    Serial.println("BELOK KIRI");
+    bot.belokKiri(2);
+  }
+  else if(valLineL == 0 && valLineR == 1){
+    Serial.println("BELOK KANAN");
+    bot.belokKanan(2);
+  }
+  
+  Serial.println(); delay(1000);
 }
--- _README.txt
+++ _README.txt
@@ -5,16 +5,25 @@ Rule:
 - Koki meletakkan makanan ke rak pada robot
 - Input keypad (nomor meja)
 	Urutan nomor meja disimpan di array
+	Input ditampilkan di LCD
+	Ada tombol nomor, reset, submit
+	Setelah SUBMIT, robot mulai jalan
 - Robot menuju ke nomor meja
 	Cara agar robot hanya menuju ke nomor meja tertentu saja?
-- Setelah robot berhenti, 
+- Setelah robot berhenti di meja X, 
 	LED pada rak makan tertentu nyala
  	LCD memberi tahu nama menu makanan untuk meja X
 - Setelah makanan diambil,
-	Notifikasi LCD untuk tekan tombol SUDAH
+	Notifikasi LCD muncul
+	Klik button lanjut
+	Ubah array yang menunjukkan tujuan robot
+- Setelah selesai mengantarkan semua,
+	Artinya, array tujuan robot [0,0,...]
+	Segera kembali ke posisi standby di dapur
 	
 
 Alur Customer:
-- Masuk retoran
+- Masuk resto
 - Menuju ke bagian pemesanan
-- 
+- Tunggu robot datang ke meja nomor X
+- Keluar resto
